7bf25834b6f9c17018dd09d0f0dd6d42155307a9
Yunzu <ab.mssaa@gmail.com>
2021-06-14 19:30:11 +0800

    firebase

--- app/src/main/java/com/example/bluetooth_b10809038/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_b10809038/MainActivity.java
@@ -1,23 +1,143 @@
 package com.example.bluetooth_b10809038;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button entry_button;
+    Activity context = MainActivity.this;
+    private TextView t1,t2;
+    private EditText et1, et2, et3, et4;
+    private Button sign_up, sign_in;
+    String email;
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        entry_button = (Button)findViewById(R.id.entry_button);
-        entry_button.setOnClickListener(v -> {
-            Intent intent = new Intent();
-            intent.setClass(MainActivity.this, ScannerActivity.class);
-            startActivity(intent);
+
+        mAuth = FirebaseAuth.getInstance();  // Initialize Firebase Auth
+        sign_in = (Button)findViewById(R.id.sign_in_button);
+        sign_up = (Button)findViewById(R.id.sign_up_button);
+
+        et1 = (EditText)findViewById(R.id.et1);
+        et2 = (EditText)findViewById(R.id.et2);
+        onStart();
+        sign_up.setOnClickListener(v -> createAccount(et1.getText().toString(),et2.getText().toString()));
+
+        sign_in.setOnClickListener(v -> {
+            signIn(et1.getText().toString(),et2.getText().toString());
+
+//            Intent intent = new Intent();
+//            intent.setClass(MainActivity.this, ScannerActivity.class);
+//            startActivity(intent);
         });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(context, new OnCompleteListener<AuthResult>(){
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(MainActivity.this, "註冊成功，請重新登入，並且開啟藍芽功能。",
+                                    Toast.LENGTH_SHORT).show();
+                            et1.setText("");
+                            et2.setText("");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(context, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            Toast.makeText(MainActivity.this, "登入成功。",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            Intent intent = new Intent();
+                            intent.setClass(MainActivity.this, ScannerActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void sendEmailVerification() {
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Email sent
+                    }
+                });
+        // [END send_email_verification]
+    }
+
+    private void reload() { }
+
+    private void updateUI(FirebaseUser user) {
+
+    }
+
+
 }
